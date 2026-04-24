d8701c0b18d711aa4d78a1d12a5455c3e2871055
Mity087 <nguyenminhtri080706@gmail.com>
2026-04-24 10:53:00 +0700

    bai 1 2 3 4

--- DeQuy.py
+++ DeQuy.py
@@ -15,4 +15,21 @@ def gt(n):
         return n *gt(n-1)
 n = int (input("nhập một số nguyên dương:"))
 print("Giai thừa của",n,"là:",gt(n))
-    
+#cau 3
+def luy_thua(x,n):
+    if n == 0:
+        return 1
+    else:
+        return x * luy_thua(x,n-1)
+    a = int(input("Nhập cơ số:"))
+    b = int(input("Nhập số mũ:"))
+    print(a,"^",b,"=",luy_thua(a,b))
+ #cau 4
+def gcd(a,b):
+        if b == 0:
+            return a
+        else:
+            return gcd(b,a%b)
+a = int(input("Nhập số nguyên dương thứ nhất:"))
+b = int(input("Nhâp số nguyên dương thứ hai"))
+print("Ước chung lớn nhất của",a ,"và",b,"là:", gcd(a,b))        
