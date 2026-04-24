0e2b46699ea6cb87b8cbeccf7a2794ceb5989d7b
Mity087 <nguyenminhtri080706@gmail.com>
2026-04-24 10:35:20 +0700

    bai 1

---
+++ DeQuy.py
@@ -0,0 +1,8 @@
+#cau 1
+def SumDigit (n):
+    if n == 0:
+        return 0
+    else:
+        return n % 10 + SumDigit(n//10)
+n = int (input("nhập một số nguyên dương: "))    
+print("Tổng các chữ số của",n,"là:", SumDigit(n))
